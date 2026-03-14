5ca7397d06beb218de151a54c14baf91b4fcaf35
DileepaLakshan <dileepalakshandesilva101@gmail.com>
2026-03-14 23:59:13 +0530

    class add to driver

--- src/main/java/com/retail/OnlineRetailDriver.java
+++ src/main/java/com/retail/OnlineRetailDriver.java
@@ -7,7 +7,9 @@ public class OnlineRetailDriver {
         int exitCode = -1;
         ProgramDriver pgd = new ProgramDriver();
         try {
-
+            pgd.addClass("salespercountry", SalesPerCountry.class, "Calculate total sales per country");
+            pgd.addClass("mostsoldproducts", MostSoldProducts.class, "Identify most sold products");
+            pgd.addClass("mostactivecustomers", MostActiveCustomers.class, "Identify most active customers");
             exitCode = pgd.run(argv);
         }
         catch(Throwable e) {
